686cf866d29b02b314a6ec289c44a9ad057bc230
salvadorec <salvadorec@iMac-de-salvadorec.local>
2019-08-26 14:48:54 -0700

    seeders configurados correctamente

--- citas.app/database/seeds/DatabaseSeeder.php
+++ citas.app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RolesAndPermissions::class);
+        $this->call(RolesAndPermissionsSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
 }
--- citas.app/database/seeds/RolesAndPermissionsSeeder.php
+++ citas.app/database/seeds/RolesAndPermissionsSeeder.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use App\User;
 
-
-class RolesAndPermissions extends Seeder
+class RolesAndPermissionsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
