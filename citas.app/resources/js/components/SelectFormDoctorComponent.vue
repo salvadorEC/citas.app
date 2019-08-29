fc5379d5eacb4c3997d94548047c5bdc48c71b2e
salvadorec <salvadorec@iMac-de-salvadorec.local>
2019-08-28 19:08:21 -0700

    SelectFormDoctorComponent listo, solo se muestran los doctores y solo el rol recepcion lo puede usar

---
+++ citas.app/resources/js/components/SelectFormDoctorComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-white-50 bg-dark">
+                      Objeto7070: Elegir Doctor
+                    </div>
+                    <div class="card-body">
+                      <div>
+                        <b-form-select v-model="seleccionado" class="mb-3">
+                          <!-- This slot appears above the options from 'options' prop -->
+                          <template slot="first">
+                            <option :value="null" disabled>-- seleccione un doctor --</option>
+                          </template>
+                          <!-- These options will appear after the ones from 'options' prop -->
+                          <option v-for="doctor in options" :key="doctor.id">{{doctor.name}}</option>
+                        </b-form-select>
+                        <div class="mt-3">Doctor seleccionado: <strong>{{ seleccionado }}</strong></div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        seleccionado: null,
+        options: []
+      }
+    },
+    created(){
+      axios.get('./api/user')
+       .then(response => this.options = response.data);
+    }
+    }
+</script>
--- citas.app/resources/views/home.blade.php
+++ citas.app/resources/views/home.blade.php
@@ -1,23 +1,15 @@
 @extends('layouts.app')
 
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  @role('recepcion')
+  @can('ver_doctores')
+    <!-- Objeto7070: Elegir Doctor-->
+    <selectformdoctor-component></selectformdoctor-component>
+    <!-- Objeto7070: Elegir Doctor-->
+  @endcan
+  @endrole
+
 @endsection
